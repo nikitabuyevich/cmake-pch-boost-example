4e5ccdf8f2fcd65d6bdaa559199c66ed89fcd2c2
Nikita Buyevich <nikita.buyevich@gmail.com>
2020-04-28 22:22:24 -0500

    feat: add attempt at pch

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -2,9 +2,12 @@
 #include <fmt/format.h>
 #include <foobar.h>
 
+#include <boost/accumulators/accumulators.hpp>
+
 int main() {
   std::cout << "Hello, World!" << std::endl;
   fmt::print("Meanie: {}, Moment: {}\n", 1, 2);
+  fmt::print("REEEEEE2\n");
   fmt::print("Yay {}", auga("ree"));
   return 0;
 }
--- pkg/int/foobar/foobar.cpp
+++ pkg/int/foobar/foobar.cpp
@@ -1,5 +1,16 @@
 #include "foobar.h"
 
+namespace ba = boost::accumulators;
+
 int auga(const std::string &str) noexcept {
-  return 1337;
+  return 1343;
+}
+
+std::tuple<double, double> accumulate_vector(const std::vector<double> &values) {
+
+  ba::accumulator_set<double, ba::stats<ba::tag::mean, ba::tag::moment<2>>> acc;
+
+  std::for_each(std::begin(values), std::end(values), std::ref(acc));
+
+  return {ba::mean(acc), ba::moment<2>(acc)};
 }
