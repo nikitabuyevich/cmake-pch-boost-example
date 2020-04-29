90cc11839f49798cb44e2b50cd428fa296de165a
Nikita Buyevich <nikita.buyevich@gmail.com>
2020-04-28 19:48:43 -0500

    fix: foobar not being visible to consumers

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "fmt/format.h"
+#include "foobar.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+  std::cout << "Hello, World!" << std::endl;
+  fmt::print("Meanie: {}, Moment: {}\n", 1, 2);
+  fmt::print("Yay {}", convert("ree"));
+  return 0;
 }
