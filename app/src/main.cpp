1d30f2c118f70a5c348b310760637762841def9f
Nikita Buyevich <nikita.buyevich@gmail.com>
2020-04-28 22:33:08 -0500

    refactor: boost import

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -2,12 +2,17 @@
 #include <fmt/format.h>
 #include <foobar.h>
 
-#include <boost/accumulators/accumulators.hpp>
-
 int main() {
   std::cout << "Hello, World!" << std::endl;
   fmt::print("Meanie: {}, Moment: {}\n", 1, 2);
   fmt::print("REEEEEE2\n");
-  fmt::print("Yay {}", auga("ree"));
+  fmt::print("Yay {}\n", auga("ree"));
+
+  std::vector<double> input = {1.2, 2.3, 3.4, 4.5};
+
+  auto[mean, moment] = accumulate_vector(input);
+
+  fmt::print("Mean: {}, Moment: {}\n", mean, moment);
+
   return 0;
 }
